baf38dfb9ae302fc3f89f4f153bf847c43520779
Diddylcp <mrborjabtf@gmail.com>
2022-04-01 16:09:17 +0200

    contentview
    Contentview

--- Mastermind/ContentView.swift
+++ Mastermind/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let board = [Color.gray, .gray, .gray, .gray]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            ScrollView{
+                // Here comes foreach
+                HStack{
+                    // Circles
+                    ColorCircle(myColor: board[0])
+                    ColorCircle(myColor: board[1])
+                    ColorCircle(myColor: board[2])
+                    ColorCircle(myColor: board[3])
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            }
+            HStack{
+                
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ColorCircle: View{
+    let myColor: Color
+    
+    var body: some View{
+        Circle()
+            .fill(myColor)
+            .frame(width:30, height:30)
     }
 }
+
